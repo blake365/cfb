f952811a3ec3da953904ef7f1c01a9868351d3b9
blake365 <68243611+blake365@users.noreply.github.com>
2024-09-24 20:43:28 -0400

    added env variable

--- src/app/conferences/[slug]/layout.tsx
+++ src/app/conferences/[slug]/layout.tsx
@@ -1,28 +1,29 @@
-import { TinyTeamCard } from '@/components/tiny-team-card'
+import { TinyTeamCard } from "@/components/tiny-team-card";
 
 export default async function WeeksLayout({
 	params,
 	children,
 }: {
-	params: { slug: string }
-	children: React.ReactNode
+	params: { slug: string };
+	children: React.ReactNode;
 }) {
 	const conference = await fetch(
-		`http://localhost:3001/conferences/${params.slug}`
-	)
-	const conferenceData = await conference.json()
+		`${process.env.NEXT_PUBLIC_SERVER_URL}/conferences/${params.slug}`,
+		{ next: { revalidate: 3600 } },
+	);
+	const conferenceData = await conference.json();
 
 	return (
-		<section className='flex flex-col items-center min-h-screen mx-4'>
-			<div className='w-full max-w-4xl text-center my-8'>
-				<h1 className='text-4xl font-bold mb-2'>{conferenceData.fullName}</h1>
+		<section className="flex flex-col items-center min-h-screen mx-4">
+			<div className="w-full max-w-4xl text-center my-8">
+				<h1 className="text-4xl font-bold mb-2">{conferenceData.fullName}</h1>
 			</div>
-			<div className='flex flex-row flex-wrap w-full gap-4 items-center mb-4 justify-center'>
+			<div className="flex flex-row flex-wrap w-full gap-4 items-center mb-4 justify-center">
 				{conferenceData.teams.map((team) => (
 					<TinyTeamCard key={team.id} team={team} size={12} />
 				))}
 			</div>
 			{children}
 		</section>
-	)
+	);
 }
