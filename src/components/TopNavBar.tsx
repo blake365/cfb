974bdf1b86b6dc3c05575f860b8da2bfeffcb096
blake365 <68243611+blake365@users.noreply.github.com>
2024-09-25 20:14:22 -0400

    nav adjustment

--- src/components/TopNavBar.tsx
+++ src/components/TopNavBar.tsx
@@ -1,5 +1,5 @@
-import { Button } from '@/components/ui/button'
-import { Menu, House } from 'lucide-react'
+import { Button } from "@/components/ui/button";
+import { Menu, House } from "lucide-react";
 import {
 	Sheet,
 	SheetContent,
@@ -7,52 +7,52 @@ import {
 	SheetTitle,
 	SheetTrigger,
 	SheetClose,
-} from '@/components/ui/sheet'
-import Link from 'next/link'
-import { ResponsiveComboBox } from '@/components/ResponsiveCombobox'
-import FavoriteTeams from '@/components/FavoriteTeams'
+} from "@/components/ui/sheet";
+import Link from "next/link";
+import { ResponsiveComboBox } from "@/components/ResponsiveCombobox";
+import FavoriteTeams from "@/components/FavoriteTeams";
 
 export default function TopNavBar() {
 	return (
-		<nav className='bg-muted'>
-			<div className='container mx-auto px-4 py-2'>
-				<div className='flex items-center justify-between'>
-					<Link href='/' className='text-xl font-bold sm:text-2xl'>
-						<House />
+		<nav className="bg-muted">
+			<div className="container mx-auto px-4 py-2">
+				<div className="flex items-center justify-between">
+					<Link href="/" className="text-xl font-bold sm:text-2xl">
+						<House className="w-9 h-9 hover:fill-current" />
 					</Link>
 
 					{/* Desktop search */}
-					<div className='hidden sm:flex items-center space-x-2'>
+					<div className="hidden sm:flex items-center space-x-2">
 						<FavoriteTeams />
-						<ResponsiveComboBox label='Find A Team' />
-						<Link href='/conferences' className='hover:underline shadow-md'>
-							<Button variant='outline'>Conferences</Button>
+						<ResponsiveComboBox label="Find A Team" />
+						<Link href="/conferences" className="hover:underline shadow-md">
+							<Button variant="outline">Conferences</Button>
 						</Link>
 					</div>
 
 					{/* Mobile menu and search */}
-					<div className='flex items-center space-x-2 sm:hidden'>
-						<div className='w-full overflow-x-scroll'>
+					<div className="flex items-center space-x-2 sm:hidden">
+						<div className="w-full overflow-x-scroll">
 							<FavoriteTeams />
 						</div>
 						<Sheet>
 							<SheetTrigger asChild>
-								<Button variant='ghost' size='icon'>
-									<Menu className='h-5 w-5' />
-									<span className='sr-only'>Open menu</span>
+								<Button variant="ghost" size="icon">
+									<Menu className="h-5 w-5" />
+									<span className="sr-only">Open menu</span>
 								</Button>
 							</SheetTrigger>
-							<SheetContent side='right'>
+							<SheetContent side="right">
 								<SheetHeader>
 									<SheetTitle>Menu</SheetTitle>
 								</SheetHeader>
-								<div className='mt-4 space-y-4'>
-									<ResponsiveComboBox label='Find A Team' />
+								<div className="mt-4 space-y-4">
+									<ResponsiveComboBox label="Find A Team" />
 									<SheetClose asChild>
-										<Link href='/conferences' className='mt-4'>
+										<Link href="/conferences" className="mt-4">
 											<Button
-												variant='outline'
-												className='w-full justify-center mt-4 shadow-md'
+												variant="outline"
+												className="w-full justify-center mt-4 shadow-md"
 											>
 												Conferences
 											</Button>
@@ -65,5 +65,5 @@ export default function TopNavBar() {
 				</div>
 			</div>
 		</nav>
-	)
+	);
 }
--- src/components/avatar-group.tsx
+++ src/components/avatar-group.tsx
@@ -1,21 +1,21 @@
-'use client'
+"use client";
 
-import React from 'react'
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
-import Link from 'next/link'
+import React from "react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import Link from "next/link";
 interface AvatarData {
-	id: number
-	logo: string
-	name: string
-	abbreviation: string
-	primaryColor: string
-	secondaryColor: string
+	id: number;
+	logo: string;
+	name: string;
+	abbreviation: string;
+	primaryColor: string;
+	secondaryColor: string;
 }
 
 interface AvatarGroupProps {
-	avatars?: AvatarData[]
-	maxAvatars?: number
-	spread?: boolean
+	avatars?: AvatarData[];
+	maxAvatars?: number;
+	spread?: boolean;
 }
 
 export function AvatarGroupComponent({
@@ -24,19 +24,15 @@ export function AvatarGroupComponent({
 }: AvatarGroupProps) {
 	const displayAvatars = Array.isArray(avatars)
 		? avatars.slice(0, maxAvatars)
-		: []
+		: [];
 
 	return (
-		<div className='overflow-x-auto max-w-[calc(100vw-10rem)]'>
-			<div className='flex items-center'>
-				<div className='flex -space-x-4 sm:hover:space-x-2 hover:space-x-0 transition-all duration-300 ease-in-out'>
+		<div className="overflow-x-auto max-w-[calc(100vw-10rem)]">
+			<div className="flex items-center">
+				<div className="flex -space-x-4 sm:hover:space-x-2 hover:space-x-0 transition-all duration-300 ease-in-out">
 					{displayAvatars.map((avatar) => (
-						<Link
-							href={`/teams/${avatar.name}`}
-							key={avatar.id}
-							className='my-1'
-						>
-							<Avatar className='w-10 h-10 border border-background hover:z-10 bg-background hover:border-primary shadow-md'>
+						<Link href={`/teams/${avatar.name}`} key={avatar.id} className="">
+							<Avatar className="w-10 h-10 border border-background hover:z-10 bg-background hover:border-primary shadow-md">
 								<AvatarImage src={avatar.logo} alt={avatar.name} />
 								{/* <AvatarFallback
 									style={{
@@ -52,5 +48,5 @@ export function AvatarGroupComponent({
 				</div>
 			</div>
 		</div>
-	)
+	);
 }
