efe747ecb14e5eef0ced7b7511af19be5dc7f394
blake365 <68243611+blake365@users.noreply.github.com>
2024-11-02 20:03:58 -0400

    records cron fix

--- api/cron/records.js
+++ api/cron/records.js
@@ -3,7 +3,7 @@ export const edge = true
 
 export default async function handler(request, response) {
 
-	const result = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/teams/updateRecords`)
+	const result = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/teams/updateRecords/hello`)
 
 	return response.status(result.status)
 }
